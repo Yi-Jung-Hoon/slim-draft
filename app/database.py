5ea192144a4db2b5e2018a333001f857ef5c6ddf
qrisma <hoony77.lee@gmail.com>
2024-07-22 10:15:03 +0900

    oracle cloud DB 접속하는 방식으로 수정

--- app/database.py
+++ app/database.py
@@ -13,10 +13,11 @@ def get_db_connection():
     user = os.getenv("ORACLE_USER")
     password = os.getenv("ORACLE_PASSWORD")
 
-    logger.debug(f"connection info : {host}, {port}, {service_name}")
+    # logger.debug(f"connection info : {host}, {port}, {service_name}")
 
-    dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
-    logger.debug(f"dsn : {dsn}")
-
+    # dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
+    # logger.debug(f"dsn : {dsn}")
+    dsn = "peru_high"
+    logger.debug(f"connection info : {user}, {dsn}, {password}")
     connection = cx_Oracle.connect(user=user, password=password, dsn=dsn)
     return connection
